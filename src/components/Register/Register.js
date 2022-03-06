c83d8108e19a101606933860edab0806e5f8b70a
haimhamiel <haim.hamiel@gmail.com>
2022-03-06 21:12:11 +0200

    security changes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -34,7 +34,7 @@ class Register extends React.Component {
         })
         .then(Response => Response.json())
         .then(user => {
-            if(user){
+            if(user.id){
                 this.props.loadUser(user);
                 this.props.onRouteChange('home');
             }
